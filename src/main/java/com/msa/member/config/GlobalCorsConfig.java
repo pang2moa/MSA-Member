81089a30a4c4306cfef3d69f382d5cdef4fafa93
pangmoa <vkdahdk@gmail.com>
2024-07-30 23:04:16 +0900

    jenkins file 추가

--- src/main/java/com/msa/member/config/GlobalCorsConfig.java
+++ src/main/java/com/msa/member/config/GlobalCorsConfig.java
@@ -4,12 +4,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class GlobalCorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*") // Next.js 앱의 URL
+                .allowedOrigins("http://localhost:3000", "http://222.108.42.200:3000", "http://222.108.42.200") // Next.js 앱의 URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
